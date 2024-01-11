ca17dcd3d8f44a0728013bfc4bc7d7cebfd75533
yonecoko <uve053ver@gmail.com>
2024-01-11 23:44:24 +0900

    ゲーム終了まで記述完了

--- main.js
+++ main.js
@@ -1,38 +1,82 @@
-// const gameArea = document.querySelector(".gameArea");
+const cardArea = document.querySelector(".gameArea");
 
-// // 1.8枚のカード生成
-// // 2.ランダムに1-4を2つずつ、数字をランダムに生成
-// // 3.8枚のカードに数字をランダムに表示
-// // 4.カードを押したら数字が見える
-// // 5.2枚のカードを押して、数字が一致したら2枚消える
-// // 6.一致しなかったら裏返す
-// // 7.カードがなくなるまで繰り返し、なくなったら「終了です」のアラート
-// // 8.アラートが出たらリロードされてカードを復活
+let numArr = [];
+let pair = [];
 
-// // 1.8枚のカード生成
-// // gameAreaのdivタグ持ってきたから、その子要素にinputタグを8つ生成
-// // inputタグを生成,gameAreaの子要素に指定
+const createCards = function () {
+  for (let i = 0; i < 8; i++) {
+    const card = document.createElement("div");
+    cardArea.appendChild(card);
+    card.classList = "back";
+  }
+};
 
-// const makeCard = function () {
-//   for (let i = 0; i < 8; i++) {
-//     const card = document.createElement("input");
-//     gameArea.appendChild(card);
-//     card.type = "button";
-//     card.classList = "card" + [i + 1];
-//     card.value = 1;
-//   }
-// };
+const shaffleCard = function (shaffleArr) {
+  for (let i = shaffleArr.length - 1; 0 < i; i--) {
+    let r = Math.floor(Math.random() * (i + 1));
 
-// makeCard();
-// // 1から4の数値を配列に入れて
-// const cardsArray = [];
-// const cardNum = Math.floor(Math.random() * 5);
+    let tmp = shaffleArr[i];
+    shaffleArr[i] = shaffleArr[r];
+    shaffleArr[r] = tmp;
+  }
+  return shaffleArr;
+};
 
-// [...cardsArray(5)].map(() => cardNum());
-// // for (let j = 1; j < 5; j++) {
-// //   const cardNum1 = Math.floor(Math.random() * 5);
-// //   cardsArray.push(cardNum1);
-// //   console.log(cardNum1);
-// // }
+const createNum = function (length) {
+  for (let i = 0; i < length; i++) {
+    numArr.push(i + 1);
+    numArr.push(i + 1);
+  }
 
-// console.log(cardsArray);
+  shaffleCard(numArr);
+};
+
+const deletePair = function () {
+  pair.pop();
+  pair.pop();
+};
+
+const btn = function () {
+  const backs = Array.from(document.querySelectorAll(".back"));
+  let count = 0;
+
+  for (let i = 0, back; (back = backs[i]); i++) {
+    back.addEventListener("click", function () {
+      backs[i].textContent = numArr[i];
+      backs[i].classList = "front";
+      pair.push(numArr[i]);
+
+      if (pair.length === 2) {
+        const fronts = Array.from(document.querySelectorAll(".front"));
+        if (pair[0] === pair[1]) {
+          setTimeout(() => {
+            fronts[0].classList = "finish";
+            fronts[1].classList = "finish";
+            deletePair();
+            count++;
+
+            setTimeout(() => {
+              if (count === 4) {
+                alert("終了です");
+                window.location.reload();
+              }
+            }, 500);
+          }, 500);
+        } else {
+          setTimeout(() => {
+            fronts.map((front) => {
+              front.classList = "back";
+              front.textContent = "";
+            });
+            deletePair();
+          }, 500);
+        }
+        // console.log(count);
+      }
+    });
+  }
+};
+
+createCards();
+createNum(4);
+btn();
